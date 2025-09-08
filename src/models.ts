89e1594e334308d3d2b8e984b1780de2cba9375c
BeliyDot <artemonchannel2005@gmail.com>
2025-09-08 10:32:21 +0300

    Implement User classes and interfaces

--- src/models.ts
+++ src/models.ts
@@ -1,25 +1,59 @@
 export interface IBook {
-    name: string,
-    author: string,
-    releaseYear: number,
+    getName(): string,
+    getAuthor(): string,
+    getReleaseYear(): number,
 }
 
 export class Book implements IBook {
-    name: string;
-    author: string;
-    releaseYear: number;
+    private name: string;
+    private author: string;
+    private releaseYear: number;
     
     constructor(name: string, author: string, releaseYear: number) {
         this.name = name;
         this.author = author;
         this.releaseYear = releaseYear;
     }
+    
+    getName(): string {
+        return this.name;
+    }
+
+    getAuthor(): string {
+        return this.author;
+    }
+
+    getReleaseYear(): number {
+        return this.releaseYear;
+    }
 }
 
 export interface IUser {
-
+    getId(): number,
+    getName(): string,
+    getEmail(): string,
 }
 
 export class User implements IUser {
+    private id: number;
+    private name: string;
+    private email: string;
 
+    constructor(id: number, name: string, email: string) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    
+    getId(): number {
+        return this.id;
+    }
+    
+    getName(): string {
+        return this.name;
+    }
+    
+    getEmail(): string {
+        return this.email;
+    }
 }
