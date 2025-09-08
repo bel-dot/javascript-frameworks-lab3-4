3798acb8bd6bda5021b1539a3d404c6a59caa517
BeliyDot <artemonchannel2005@gmail.com>
2025-09-08 11:13:39 +0300

    Implement Storage class to manage localStorage

--- src/storage.ts
+++ src/storage.ts
@@ -1,3 +1,17 @@
 export class Storage {
-
+    static set(key: string, value: string): void {
+        localStorage.setItem(key, value);
+    }
+    
+    static get(key: string): void {
+        localStorage.getItem(key);
+    }
+    
+    static remove(key: string): void {
+        localStorage.removeItem(key);
+    }
+    
+    static clear(): void {
+        localStorage.clear();
+    }
 }
