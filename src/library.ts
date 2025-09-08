7525b166baa1c74cf7a3cd78c02f6690b0930b3f
BeliyDot <artemonchannel2005@gmail.com>
2025-09-08 10:47:48 +0300

    Implement Library class for managing object collections

--- src/library.ts
+++ src/library.ts
@@ -1,3 +1,21 @@
 export class Library<T> {
+    private objects: T[] = [];
+
+    constructor(objects: T[]) {
+        this.objects = objects;
+    }
     
+    add(obj: T): void {
+        this.objects.push(obj);
+    }
+    
+    remove(obj: T): void {
+        const index = this.objects.indexOf(obj);
+        
+        this.objects.splice(index, 1);
+    }
+    
+    search(prop: string, predicate: () => string): T | undefined {
+        return this.objects.find(predicate);
+    }
 }
