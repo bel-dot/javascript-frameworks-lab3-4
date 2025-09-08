e5b4aa539dd33daa5d86ddc39783db53ad195503
BeliyDot <artemonchannel2005@gmail.com>
2025-09-08 10:26:20 +0300

    Implement Book classes and interfaces

--- src/models.ts
+++ src/models.ts
@@ -1,9 +1,19 @@
 export interface IBook {
-
+    name: string,
+    author: string,
+    releaseYear: number,
 }
 
 export class Book implements IBook {
-
+    name: string;
+    author: string;
+    releaseYear: number;
+    
+    constructor(name: string, author: string, releaseYear: number) {
+        this.name = name;
+        this.author = author;
+        this.releaseYear = releaseYear;
+    }
 }
 
 export interface IUser {
