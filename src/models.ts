32f76cde1a23c10eaa73c0a1eccdde92bdb662fb
bel-dot <artemonchannel2005@gmail.com>
2025-10-06 00:51:07 +0300

    Return to encapsulation with Book and User classes

--- src/models.ts
+++ src/models.ts
@@ -1,20 +1,43 @@
 export interface IBook {
-    name: string,
-    author: string,
-    releaseYear: number,
-    borrower: number,
+    getId: number,
+    getName: string,
+    getAuthor: string,
+    getReleaseYear: number,
+    isBorrowed(): boolean,
 }
 
 export class Book implements IBook {
-    name: string;
-    author: string;
-    releaseYear: number;
-    borrower: number = 0;
+    private id: number;
+    private name: string;
+    private author: string;
+    private releaseYear: number;
+    private borrower: number = 0;
     
-    constructor(name: string, author: string, releaseYear: number) {
+    constructor(id: number, name: string, author: string, releaseYear: number) {
+        this.id = id;
         this.name = name;
         this.author = author;
         this.releaseYear = releaseYear;
+    }
+    
+    get getId(): number {
+        return this.id;
+    }
+    
+    get getName(): string {
+        return this.name;
+    }
+    
+    get getAuthor(): string {
+        return this.author;
+    }
+    
+    get getReleaseYear(): number {
+        return this.releaseYear;
+    }
+    
+    get getBorrower(): number {
+        return this.borrower;
     }
     
     isBorrowed(): boolean {
@@ -31,16 +54,16 @@ export class Book implements IBook {
 }
 
 export interface IUser {
-    id: number,
-    name: string,
-    email: string
+    getId: number,
+    getName: string,
+    getEmail: string
 }
 
 export class User implements IUser {
-    id: number;
-    name: string;
-    email: string;
-    private borrowedBooks: Book[] = [];
+    private id: number;
+    private name: string;
+    private email: string;
+    private borrowedBooks: number = 0;
 
     constructor(id: number, name: string, email: string) {
         this.id = id;
@@ -48,22 +71,27 @@ export class User implements IUser {
         this.email = email;
     }
     
-    borrowBook(book: Book): boolean {
-        if(this.borrowedBooks.length > 3 || book.isBorrowed()) {
-            return false;
-        }
-        this.borrowedBooks.push(book);
-        book.borrow(this.id);
-        return true;
+    get getId(): number {
+        return this.id;
     }
     
-    returnBook(book: Book): boolean {
-        const index = this.borrowedBooks.indexOf(book);
-        if(index != -1) {
-            this.borrowedBooks.splice(index, 1);
-            book.return();
-            return true;
+    get getName(): string {
+        return this.name;
+    }
+    
+    get getEmail(): string {
+        return this.email;
+    }
+    
+    public borrowBook(book: Book): void {
+        if(this.borrowedBooks > 3 || book.isBorrowed()) {
+            throw new Error('Too many books borrowed or book is already borrowed');
         }
-        return false;
+        this.borrowedBooks++;
+        book.borrow(this.id);
+    }
+    
+    public returnBook(): void {
+        this.borrowedBooks--;
     }
 }
